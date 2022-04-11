45ba4f3dedb08cb09551170e949c3b811f9a3cc5
unisitohang17 <102931767+unisitohang17@users.noreply.github.com>
2022-04-11 17:03:13 +0700

    Add files via upload

---
+++ db.js
@@ -0,0 +1,80 @@
+const JADWAL = [
+    {
+      jadwal_id: "01",
+      bandara_kode_keberangkatan: "KNO",
+      bandara_kode_tujuan: "PLM",
+      maskapai_id: "GA",
+      jadwal_keberangkatan: "02 Februari 2022",
+    },
+    {
+      jadwal_id: "02",
+      bandara_kode_keberangkatan: "PLM",
+      bandara_kode_tujuan: "KNO",
+      maskapai_id: "ID",
+      jadwal_keberangkatan: "02 Februari 2022",
+    },
+    {
+      jadwal_id: "03",
+      bandara_kode_keberangkatan: "PLM",
+      bandara_kode_tujuan: "TKG",
+      maskapai_id: "QG",
+      jadwal_keberangkatan: "02 Februari 2022",
+    },
+    {
+      jadwal_id: "04",
+      bandara_kode_keberangkatan: "KNO",
+      bandara_kode_tujuan: "DTB",
+      maskapai_id: "SJ",
+      jadwal_keberangkatan: "02 Februari 2022",
+    },
+    {
+      jadwal_id: "05",
+      bandara_kode_keberangkatan: "DTB",
+      bandara_kode_tujuan: "KNO",
+      maskapai_id: "QG",
+      jadwal_keberangkatan: "02 Februari 2022",
+    },
+  ];
+  
+  const MASKAPAI = [
+    {
+      maskapai_id: "ID",
+      maskapai_nama: "Batik Air",
+      maskapai_logo: " ",
+    },
+    {
+      maskapai_id: "GA",
+      maskapai_nama: "Garuda Indonesia",
+      maskapai_logo: " ",
+    },
+    {
+      maskapai_id: "SJ",
+      maskapai_nama: "Sriwijaya Air",
+      maskapai_logo: " ",
+    },
+    {
+      maskapai_id: "QG",
+      maskapai_nama: "Citilink",
+      maskapai_logo: " ",
+    },
+  ];
+  
+  const BANDARA = [
+    {
+      bandara_kode: "PLM",
+      bandara_nama: "Sultan Mahmud Badaruddin",
+    },
+    {
+      bandara_kode: "KNO",
+      bandara_nama: 'Kualanamu',
+    },
+    {
+      bandara_kode: "TKG",
+      bandara_nama: "Radin Inten",
+    },
+    {
+      bandara_kode: "DTB",
+      bandara_nama: "Silangit",
+    },
+  ];
+  export {JADWAL, MASKAPAI, BANDARA}
